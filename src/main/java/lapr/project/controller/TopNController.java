a25a5bc16a2a386b7ccf00c7b9c08d362f883642
Violante 1201284 <1201284@isep.ipp.pt>
2021-11-04 11:48:33 +0000

    G54-6 #comment important correction in SD and TopNController class created

--- src/main/java/lapr/project/controller/TopNController.java
+++ src/main/java/lapr/project/controller/TopNController.java
@@ -1,4 +1,67 @@
 package lapr.project.controller;
 
+import lapr.project.domain.BST.ShipsBTS;
+import lapr.project.domain.model.Company;
+import lapr.project.domain.model.Ship;
+import lapr.project.domain.model.VesselType;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 public class TopNController {
+    /**
+     * The company associated to the Controller.
+     */
+    private Company company;
+
+    /**
+     * The binary search Tree with the ships imported from the file
+     */
+    private ShipsBTS shipBST;
+
+    /**
+     * empty constructor for the TopNController Class
+     */
+    public TopNController(){
+        this.company= App.getInstance().getCompany();
+        this.shipBST = company.getShipBST();
+    }
+
+    /**
+     * Constructor for TopNController Class, receiving "company" as attribute
+     * @param company
+     */
+    public TopNController(Company company) {
+        this.company = company;
+        this.shipBST = company.getShipBST();
+    }
+
+    /**
+     * Method to get all the ships within the Base Date Time gap
+     * @param initialDate initial Base Date Time
+     * @param finalDate final Base Date Time
+     * @return list with all the ships who belong in the time gap
+     */
+    public List<Ship> getSipsByDate(Date initialDate, Date finalDate){
+        return this.shipBST.getShipsByDate(initialDate, finalDate);
+    }
+
+    /**
+     * method to sort the ships with the most km travelled
+     * @param shipList list with the ships belonging in the time gap
+     * @param number number of ships to be sorted
+     */
+    public void sortNShips(List<Ship> shipList, int number) {
+        this.shipBST.sortNShips(shipList, number);
+    }
+
+    /**
+     * method to get the map with the ships associated by VesselType and sorted
+     * @param sortedShips list with the sorted ships by most km travelled
+     * @return map with the ships associated by VesselType and sorted
+     */
+    public Map<VesselType, List<Ship>> getShipWithMean(List<Ship> sortedShips) {
+        return this.shipBST.getShipWithMean(sortedShips);
+    }
 }
