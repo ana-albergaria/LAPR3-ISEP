480f3e822eecbd4b453457658559d33a22e789d2
João Lucas Wolff <1200049@isep.ipp.pt>
2021-11-14 10:51:59 +0000

    G54-9 #comment adding names to check constraints

--- docs/US109/physical_data_model_script.sql
+++ docs/US109/physical_data_model_script.sql
@@ -1,13 +1,15 @@
 create table country(
 	country_name varchar(70) constraint pk_country PRIMARY KEY NOT NULL,
 	continent varchar(30) NOT NULL
-);
+	);
 
 create table placeLocation(
-	locationLatitude numeric(6,4) NOT NULL CHECK(locationLatitude >= -90 AND locationLatitude <= 90),
-	locationLongitude numeric(7,4) NOT NULL CHECK(locationLongitude >= -180 AND locationLongitude <= 180),
+	locationLatitude numeric(6,4) NOT NULL ,
+	locationLongitude numeric(7,4) NOT NULL ,
 	country varchar(70) NOT NULL,
 
+    constraint chk_longitude CHECK(locationLongitude >= -180 AND locationLongitude <= 180),
+    constraint chk_latitude CHECK(locationLatitude >= -90 AND locationLatitude <= 90),
 	constraint pk_placelocation_coordinates PRIMARY KEY (locationLatitude, locationLongitude),
 	constraint fk_placeLocation_country FOREIGN KEY (country) references country(country_name)
 );
@@ -32,11 +34,14 @@ create table vesselType(
 create table ship(
 	mmsi integer constraint pk_ship PRIMARY KEY NOT NULL,
 	vesselTypeId integer UNIQUE NOT NULL,
-	imo varchar(10) UNIQUE NOT NULL check(imo like 'IMO%'),
+	imo varchar(10) UNIQUE NOT NULL ,
 	callSign varchar(4) UNIQUE NOT NULL,
 	shipName varchar(30) NOT NULL,
 	currentCapacity numeric(6,2),
-	draft numeric(5,2) NOT NULL check(draft > 0),
+	draft numeric(5,2) NOT NULL ,
+
+	constraint chk_imo check(imo like 'IMO%'),
+	constraint chk_draft check(draft > 0),
 	constraint fk_ship_vesselType FOREIGN KEY (vesselTypeId) references vesselType(vesselTypeId)
 );
 
@@ -46,8 +51,9 @@ create table cargoManifest(
 	port_id integer NOT NULL,
 	mmsi integer NOT NULL,
 	typeOfTransport varchar(10) NOT NULL,
-	status varchar(12) NOT NULL check(status like 'loading' OR status like 'unloading'),
+	status varchar(12) NOT NULL ,
 
+    constraint chk_status check(status like 'loading' OR status like 'unloading'),
 	constraint fk_cargoManifest_port FOREIGN KEY (port_id) references port(port_id),
 	constraint fk_cargoManifest_ship FOREIGN KEY (mmsi) references ship(mmsi)
 );
@@ -55,14 +61,16 @@ create table cargoManifest(
 
 create table shipPosition(
 	baseDateTime date NOT NULL,
-	latitude numeric(6,4) NOT NULL check((latitude >= -90 AND latitude <= 90) OR latitude = 91),
-	longitude numeric(7,4) NOT NULL check((longitude >= -180 AND longitude <= 180) OR longitude = 181),
+	latitude numeric(6,4) NOT NULL ,
+	longitude numeric(7,4) NOT NULL,
 	mmsi integer NOT NULL,
 	sog numeric(5,2) NOT NULL,
 	cog numeric(5,2) NOT NULL check(cog >=0 AND cog <= 359),
 	heading numeric(5,2) NOT NULL check((heading >=0 AND heading <= 359) OR heading = 511),
 	transceiver varchar(15) NOT NULL,
 
+	constraint chk_latitude_ship_position check((latitude >= -90 AND latitude <= 90) OR latitude = 91),
+	constraint chk_longitude_ship_position check((longitude >= -180 AND longitude <= 180) OR longitude = 181),
 	constraint fk_shipPosition_ship FOREIGN KEY (mmsi) references ship(mmsi),
 	constraint pk_shipPosition PRIMARY KEY (baseDateTime, latitude, longitude)
 );
@@ -78,10 +86,14 @@ create table energyPower(
 
 create table container(
 	container_id integer constraint pk_container PRIMARY KEY NOT NULL,
-	payload numeric(5,2) NOT NULL check(payload >= 0),
-	tare numeric(5,2) NOT NULL check(tare >= 0),
-	gross numeric(5,2) NOT NULL check(gross >= 0),
-	isoCode varchar(4) NOT NULL
+	payload numeric(5,2) NOT NULL,
+	tare numeric(5,2) NOT NULL,
+	gross numeric(5,2) NOT NULL,
+	isoCode varchar(4) NOT NULL,
+
+	constraint chk_payload check(payload >= 0),
+	constraint chk_tare check(tare >= 0),
+	constraint chk_gross check(gross >= 0)
 );
 
 create table refrigeration(
