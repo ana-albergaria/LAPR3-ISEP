27910c29bc19e57e4a1088834d5044a64b0e7e3d
João Lucas Wolff <1200049@isep.ipp.pt>
2021-11-18 10:59:55 +0000

    G54-111 #comment adding interface, factory, databaseconnection and creating get connection in App controller #time 40m #close

--- src/main/java/lapr/project/controller/App.java
+++ src/main/java/lapr/project/controller/App.java
@@ -1,5 +1,7 @@
 package lapr.project.controller;
 
+import lapr.project.data.ConnectionFactory;
+import lapr.project.data.DatabaseConnection;
 import lapr.project.domain.model.Company;
 import auth.AuthFacade;
 import auth.UserSession;
@@ -8,7 +10,10 @@ import lapr.project.domain.shared.Constants;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,12 +23,13 @@ public class App {
 
     private Company company;
     private AuthFacade authFacade;
-
+    private Connection connection;
     private App()
     {
         this.company = new Company(Constants.PARAMS_COMPANY_DESIGNATION);
         this.authFacade = this.company.getAuthFacade();
         bootstrap();
+        /*dataBaseConnection();*/
     }
 
     public Company getCompany()
@@ -31,6 +37,34 @@ public class App {
         return this.company;
     }
 
+    private void dataBaseConnection(){
+        DatabaseConnection databaseConnection = null;
+        try {
+            databaseConnection = ConnectionFactory.getInstance().getDatabaseConnection();
+        } catch (IOException exception) {
+            Logger.getLogger("db")
+                    .log(Level.SEVERE, null, exception);
+        }
+
+        connection = databaseConnection.getConnection();
+        if(connection != null){
+            try{
+                connection.setAutoCommit(false);
+            }catch (Exception e){
+                Logger.getLogger("db")
+                        .log(Level.SEVERE, e.getMessage(), e);
+            }
+        }
+
+        System.out.println("Connected to the database!");
+    }
+
+    public Connection getConnection(){
+        if(connection == null){
+                dataBaseConnection();
+        }
+        return connection;
+    }
 
     public UserSession getCurrentUserSession()
     {
@@ -50,7 +84,6 @@ public class App {
     private void bootstrap()
     {
         this.authFacade.addUserRole(Constants.ROLE_TRAFFIC_MANAGER,Constants.ROLE_TRAFFIC_MANAGER);
-
         this.authFacade.addUserWithRole("Traffic Manager", "trafficm@mail.com", "123456",Constants.ROLE_TRAFFIC_MANAGER);
     }
 
