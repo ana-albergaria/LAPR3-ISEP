675f78cb375a5d577a75592d25df1ed6e5712a7b
martaribeiro <1201592@isep.ipp.pt>
2021-11-30 14:59:56 +0000

    G54-101 G54-102 G54-103 G54-104 #time 50m #comment Started ShipOccupancyRatesController and ShipOccupancyRatesControllerTest

---
+++ src/main/java/lapr/project/controller/ShipOccupancyRatesController.java
@@ -0,0 +1,61 @@
+package lapr.project.controller;
+
+import lapr.project.domain.BST.PositionsBST;
+import lapr.project.domain.model.Company;
+
+import java.util.Date;
+
+/**
+ * Controller class to coordinate the calculation of ship's occupancy rate
+ *
+ * @author Marta Ribeiro (1201592)
+ */
+public class ShipOccupancyRatesController {
+
+    /**
+     * Company instance of the session.
+     */
+    private Company company;
+
+    /**
+     * Constructor for the controller.
+     */
+    public ShipOccupancyRatesController(){
+        this(App.getInstance().getCompany());
+    }
+
+    /**
+     * Constructor receiving the company as an argument.
+     *
+     * @param companyy instance of company to be used.
+     */
+    public ShipOccupancyRatesController(Company companyy){
+        this.company=companyy;
+    }
+
+    public int calculateOccupancyRate(int maxCapacity, int initialNumContainers, int addedContainerNum, int removedContainersNum){
+        int current = initialNumContainers+addedContainerNum-removedContainersNum;
+        if (current>maxCapacity){
+            return -1; //when invalid
+        } else {
+            return current/maxCapacity*100;
+        }
+    }
+
+    public int getShipOccupancyRateByCargoManifestID(int cargoManifestID){
+        int maxCapacity=0, initialNumContainers=0, addedContainersNum=0, removedContainersNum=0;
+        calculateOccupancyRate(maxCapacity, initialNumContainers, addedContainersNum,removedContainersNum);
+        throw new IllegalArgumentException("to be developed");
+    }
+
+    public int getCargoManifestIDByMmsiAndDate(int mmsi, Date date){
+        throw new IllegalArgumentException("to be developed");
+    }
+
+    public int getShipOccupancyRateByMmsiAndDate(int mmsi, Date date){
+        int cargoManifestID = getCargoManifestIDByMmsiAndDate(mmsi,date);
+        getShipOccupancyRateByCargoManifestID(cargoManifestID);
+        throw new IllegalArgumentException("to be developed");
+    }
+
+}
---
+++ src/test/java/lapr/project/controller/ShipOccupancyRatesControllerTest.java
@@ -0,0 +1,41 @@
+package lapr.project.controller;
+
+import lapr.project.domain.model.Company;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ShipOccupancyRatesControllerTest {
+
+    private Company comp;
+    private ShipOccupancyRatesController ctrl;
+
+    @BeforeEach
+    public void SetUp(){
+        comp = new Company("Company");
+        this.ctrl=new ShipOccupancyRatesController(comp);
+    }
+
+    /*@Test
+    public void testOccupancyRatesCalculationWhenValid(){
+        int maxCapacity1=75;
+        int initialNumContainers1=15;
+        int addedContainerNum1=36;
+        int removedContainersNum1=12;
+        int expected=52;
+        int actual=ctrl.calculateOccupancyRate(maxCapacity1,initialNumContainers1,addedContainerNum1,removedContainersNum1);
+        Assert.assertEquals(expected,actual);
+    }*/
+
+    @Test
+    public void testOccupancyRatesCalculationWhenInvalid(){
+        int maxCapacity1=30;
+        int initialNumContainers1=15;
+        int addedContainerNum1=36;
+        int removedContainersNum1=12;
+        int expected=-1;
+        int actual=ctrl.calculateOccupancyRate(maxCapacity1,initialNumContainers1,addedContainerNum1,removedContainersNum1);
+        Assert.assertEquals(expected,actual);
+    }
+
+}
