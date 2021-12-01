c3fb7594f4665d0af2c7283ac8380344bb7559fa
Ana Albergaria <1201518@isep.ipp.pt>
2021-12-01 19:13:52 +0000

    G54-64 #time 10m #comment Adding script with US204 functions + Fixing bugs in Jenkins

--- src/main/java/lapr/project/data/ShipTripStoreDB.java
+++ src/main/java/lapr/project/data/ShipTripStoreDB.java
@@ -8,34 +8,44 @@ import java.sql.SQLException;
 import java.sql.Types;
 
 public class ShipTripStoreDB {
-    /*
-    public static void main(String[] args) throws SQLException {
-        int shipTripId = getShipTripId(App.getInstance().getConnection(),1,5);
+
+    /*public static void main(String[] args) throws SQLException {
+        int shipTripId = getShipTripId(App.getInstance().getConnection(),1,7);
         System.out.println(shipTripId);
         String location = getLocation(App.getInstance().getConnection(), shipTripId);
         System.out.println(location);
     }
      */
 
-    public int getShipTripId(DatabaseConnection databaseConnection, int containerId, int shipmentId) throws SQLException {
-        Connection connection = databaseConnection.getConnection();
-        CallableStatement cs = connection.prepareCall("{? = call get_shiptrip_id(?, ?)}");
-        cs.setInt(2, containerId);
-        cs.setInt(3, shipmentId);
-        cs.registerOutParameter(1, Types.INTEGER);
-        cs.executeUpdate();
-        int shipTripId = cs.getInt(1);
-        return shipTripId;
 
+    public int getShipTripId(DatabaseConnection databaseConnection, int containerId, int shipmentId) {
+        try {
+            Connection connection = databaseConnection.getConnection();
+            CallableStatement cs = connection.prepareCall("{? = call get_shiptrip_id(?, ?)}");
+            cs.setInt(2, containerId);
+            cs.setInt(3, shipmentId);
+            cs.registerOutParameter(1, Types.INTEGER);
+            cs.executeUpdate();
+            int shipTripId = cs.getInt(1);
+            return shipTripId;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return -1;
     }
 
-    public String getLocation(DatabaseConnection databaseConnection, int shipTripId) throws SQLException {
-        Connection connection = databaseConnection.getConnection();
-        CallableStatement cs = connection.prepareCall("{? = call get_location(?)}");
-        cs.setInt(2, shipTripId);
-        cs.registerOutParameter(1, Types.VARCHAR);
-        cs.executeUpdate();
-        String location = cs.getString(1);
-        return location;
+    public String getLocation(DatabaseConnection databaseConnection, int shipTripId) {
+        try {
+            Connection connection = databaseConnection.getConnection();
+            CallableStatement cs = connection.prepareCall("{? = call get_location(?)}");
+            cs.setInt(2, shipTripId);
+            cs.registerOutParameter(1, Types.VARCHAR);
+            cs.executeUpdate();
+            String location = cs.getString(1);
+            return location;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        throw new UnsupportedOperationException("Some error occured. Please try again.");
     }
 }
