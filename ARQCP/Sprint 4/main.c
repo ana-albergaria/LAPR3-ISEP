64045313f01619f0247d331890568087c7230ecd
João Lucas Wolff <1200049@isep.ipp.pt>
2022-01-17 17:23:36 +0000

    G54-283 #time 1h 20m #comment adding us 410 and 411 to repository

--- ARQCP/Sprint 4/main.c
+++ ARQCP/Sprint 4/main.c
@@ -4,17 +4,15 @@
 
 int array_size;
 int maxX, maxY, maxZ;	
-int *ptr;
+float container_x=6, container_y=2.5, container_z=2.5;
 
 int main(void) {
     FILE *file = fopen("manifest2.txt", "r");
     Container *pointer = NULL;
     pointer = get_pointer(file);
+    array_size = get_arr_size(file);
 
-    // printf("id: %d\n", pointer->container_id);
-    // printf("x: %d\n", pointer->position_x);
-    // printf("y: %d\n", pointer->position_y);
-    // printf("z: %d\n", pointer->position_z);
-    // printf("Refrigerated: %d\n", pointer->refrigerated);
+    printf("needed energy for an hour (J/h)(410): %f\n", get_needed_energy_for_container(pointer, array_size, 1,1,1, 20));
+    check_enough_energy(pointer, array_size, 20.0, 20.0);
 }
 
